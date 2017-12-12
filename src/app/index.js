fbd28b6f36b94c20bd36170b8bd6a32077cb6cd6
COREA BONILLA EDWIN <COREABE@central.bccr.fi.cr>
2017-12-12 13:00:43 -0600

    loaders and plugins

--- src/app/index.js
+++ src/app/index.js
@@ -1,1 +1,11 @@
-console.log("This is the Index Page");
+import "../style/app.scss";
+
+class TestClass {
+    constructor() {
+        let msg = "Using ES2015+ syntax";
+        console.log(msg);
+        console.log(API_KEY);
+    }
+}
+
+let test = new TestClass();
--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,9 @@
-const HtmlWebpackPlugin = require("html-webpack-plugin"); // Require  html-webpack-plugin plugin
+var HtmlWebpackPlugin = require("html-webpack-plugin"); // Require  html-webpack-plugin plugin
+var ExtractTextPlugin = require("extract-text-webpack-plugin");
+var DashboardPlugin = require('webpack-dashboard/plugin');
+var webpack = require("webpack");
+
+require("dotenv").config();
 
 module.exports = {
     entry: __dirname + "/src/app/index.js", // webpack entry point. Module to start building dependency graph
@@ -8,13 +13,53 @@ module.exports = {
         publicPath: "/"
     },
     module: {  // where we defined file patterns and their loaders
-        rules: []
+        rules: [
+            {
+                test: /\.html/,
+                loader: "raw-loader"
+            },
+            {
+                test: /\.(js|jsx)$/,
+                loader: "babel-loader",
+                query: {
+                    presets: ["env", "es2015", "react", "stage-1"]
+                },
+                exclude: [
+                    /node_modules/
+                ]
+            },            
+            {
+                // regular css files
+                test: /\.css$/,
+                loader: ExtractTextPlugin.extract({
+                    fallback: "style-loader",
+                    //resolve-url-loader may be chained before sass-loader if necessary
+                    use: ["css-loader"]
+                })
+            },            
+            {
+                test: /\.(sass|scss)$/,
+                loader: ExtractTextPlugin.extract({
+                    fallback: "style-loader",
+                    //resolve-url-loader may be chained before sass-loader if necessary
+                    use: [
+                        { loader: "css-loader" }, 
+                        { loader: "sass-loader"}
+                    ]
+                })                
+            }                        
+        ]
     },
     plugins: [  // Array of plugins to apply to build chunk
         new HtmlWebpackPlugin({
             template: __dirname + "/src/public/index.html",
             inject: "body"
-        })
+        }),
+        new ExtractTextPlugin("styles.css"), // extract css to a separate file called styles.css
+        new webpack.DefinePlugin({  // plugin to define global constants
+            API_KEY: JSON.stringify(process.env.API_KEY)
+        }),
+        new DashboardPlugin()
     ],
     devServer: {  // configuration for webpack-dev-server
         contentBase: "./src/public",  //source of static assets
